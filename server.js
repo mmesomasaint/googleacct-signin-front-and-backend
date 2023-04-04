1f9e1e4c456e63a8ca9bfb26b2e5686813a707cc
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-04 06:19:58 +0000

    Fix: change newPassword to password

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/api/password', (req, res) => {
   // Update the user with his email.
   db.run(
     `UPDATE users SET password = ? WHERE email = ?`,
-    [newPassword, email],
+    [password, email],
     (err) => {
       if (err) {
         res.status(500).send('Password error')

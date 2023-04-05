44cd966c4fe7a8b8f3ab231669bd7912be65845c
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-05 02:38:31 +0000

    Feat: Design the loggedIn panel

---
+++ client/src/components/LoggedIn.tsx
@@ -0,0 +1,9 @@
+function LoggedIn() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-extrabold leading-none text-black">User Details Saved Successfully</h1>
+    </div>
+  )
+}
+
+export default LoggedIn

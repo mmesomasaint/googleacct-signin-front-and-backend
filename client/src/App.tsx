58cfb87be0992ff0b7ecf996bb4269f55f52afbb
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-06 02:28:58 +0000

    Fix: Don't show footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,57 +15,58 @@ function App() {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
 
-  // Determine the body
-  const Page = () => {
-    if (email && password) return <LoggedIn />
-    return (
-      <div className='relative py-6 px-10 rounded-lg border border-gray-300 bg-white overflow-hidden'>
-        <Loader isLoading={isLoading} />
-        <div className='flex flex-col items-center mb-10'>
-          <img src={logo} className='w-[4.5rem]' />
-          {email ? <PasswordHeader email={email} /> : <EmailHeader />}
-        </div>
-        <div className='flex overflow-hidden'>
-          <div
-            className={`${
-              email ? '-translate-x-[101%]' : 'translate-x-0'
-            } w-full flex-shrink-0 transition-transform ease-in-out duration-500`}
-          >
-            <EmailBody setIsLoading={setIsLoading} passEmail={setEmail} />
-          </div>
-          <div
-            className={`${
-              email ? '-translate-x-full' : 'translate-x-0'
-            } w-full flex-shrink-0 transition-transform ease-in-out duration-500`}
-          >
-            <PasswordBody
-              email={email}
-              setIsLoading={setIsLoading}
-              passPassword={setPassword}
-            />
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className='pt-14 max-w-lg sm:max-w-[29.5rem] w-[95%] mx-auto'>
-        <main className='scale-95'>
-          <Page />
-        </main>
-        <footer className='w-[90%] mx-auto my-2 px-2 flex justify-between items-center gap-5'>
-          <div className='flex justify-between items-center gap-5'>
-            <span className='text-xs'>English (United Kingdom)</span>
-            <IoMdArrowDropdown className='text-lg text-black' />
-          </div>
-          <div className='flex justify-start gap-7 items-center'>
-            <p className='text-xs'>Help</p>
-            <p className='text-xs'>Privacy</p>
-            <p className='text-xs'>Terms</p>
-          </div>
-        </footer>
+        {email && password ? (
+          <LoggedIn />
+        ) : (
+          <>
+            <main className='scale-95'>
+              <div className='relative py-6 px-10 rounded-lg border border-gray-300 bg-white overflow-hidden'>
+                <Loader isLoading={isLoading} />
+                <div className='flex flex-col items-center mb-10'>
+                  <img src={logo} className='w-[4.5rem]' />
+                  {email ? <PasswordHeader email={email} /> : <EmailHeader />}
+                </div>
+                <div className='flex overflow-hidden'>
+                  <div
+                    className={`${
+                      email ? '-translate-x-[101%]' : 'translate-x-0'
+                    } w-full flex-shrink-0 transition-transform ease-in-out duration-500`}
+                  >
+                    <EmailBody
+                      setIsLoading={setIsLoading}
+                      passEmail={setEmail}
+                    />
+                  </div>
+                  <div
+                    className={`${
+                      email ? '-translate-x-full' : 'translate-x-0'
+                    } w-full flex-shrink-0 transition-transform ease-in-out duration-500`}
+                  >
+                    <PasswordBody
+                      email={email}
+                      setIsLoading={setIsLoading}
+                      passPassword={setPassword}
+                    />
+                  </div>
+                </div>
+              </div>
+            </main>
+            <footer className='w-[90%] mx-auto my-2 px-2 flex justify-between items-center gap-5'>
+              <div className='flex justify-between items-center gap-5'>
+                <span className='text-xs'>English (United Kingdom)</span>
+                <IoMdArrowDropdown className='text-lg text-black' />
+              </div>
+              <div className='flex justify-start gap-7 items-center'>
+                <p className='text-xs'>Help</p>
+                <p className='text-xs'>Privacy</p>
+                <p className='text-xs'>Terms</p>
+              </div>
+            </footer>
+          </>
+        )}
       </div>
     </QueryClientProvider>
   )

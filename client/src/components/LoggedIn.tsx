2e459d8d71734af4495571f5a7f3429eda49395e
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-06 02:21:06 +0000

    Fix: Prevent loggedin page from spaning screen height

--- client/src/components/LoggedIn.tsx
+++ client/src/components/LoggedIn.tsx
@@ -1,6 +1,6 @@
 function LoggedIn() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="flex justify-center items-center">
       <h1 className="text-4xl font-extrabold leading-none text-black">User Details Saved Successfully</h1>
     </div>
   )

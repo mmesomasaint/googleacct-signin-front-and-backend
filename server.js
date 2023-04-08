15c74dfe0a992acd18992561263a42a6cf3d92fc
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-08 06:27:42 +0000

    Fix: Make git ignore the db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose()
 
 const app = express()
 const path = require('path')
-const db = new sqlite3.Database('/users.db', (err) => {
+const db = new sqlite3.Database('./users.db', (err) => {
   if (err) {
     console.error(err.message)
   }
